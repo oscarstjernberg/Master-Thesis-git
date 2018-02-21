5d432ed67b76af3e2e1ea2a245532763dd7caf78
oscarstjernberg <oscar.stjernberg@gmail.com>
2018-02-21 14:36:27 +0100

    fixed the bug

--- Arduino/logger/logger.ino
+++ Arduino/logger/logger.ino
@@ -1,5 +1,7 @@
 #include <ESP8266WiFi.h>
-#include "SD_FUNCTIONS.h"
+#include <SD.h>
+
+
 ////////////////////////////////////
 //          WIFI SETTINGS         //
 ////////////////////////////////////
@@ -15,6 +17,7 @@ char cmd[100];
 ////////////////////////////////////
 //          SD SETTINGS           //
 ///////////////////////////////////
+File BackupFile;
 int SD_pin = 53;
 
 enum months  {Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec};
@@ -67,10 +70,6 @@ void setup() {
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
 
 //////////////////// WiFi functions /////////////////////
 
@@ -94,4 +93,78 @@ Serial.println(WiFi.localIP());
 }
 
 
+//////////////////////// SD-card functions ////////////////////////////////////
+
+// INITIALIZES SD CARD
+
+void SDinit(int SD_pin)
+{
+  Serial.print(F("Initializing SD card..."));
+  if (!SD.begin(SD_pin)) {
+    Serial.println(F("Initialization SD-card failed!"));
+    while (1);
+  }
+  Serial.println(F("Initialization SD-card done."));
+
+  // Open file for writing
+  BackupFile = SD.open("Backup.txt", FILE_WRITE);
+
+  // If the file is open-write
+  if (BackupFile)
+  {
+    BackupFile.print("Time\tMeasurement");
+    BackupFile.println();
+  }
+  else {
+    Serial.println(F("ERROR: File not found"));
+  }
+  BackupFile.close();
+} // SD_init
+
+
+// WRITES CONTENT OF SG TO SD CARD  
+void SDwrite(int SG) {
+
+  String time_str;
+
+  // Open file for writing
+  BackupFile = SD.open("Backup.txt", FILE_WRITE);
+
+  // If the file is open-write
+  if (BackupFile)
+  {
+    // Write time
+    if (WiFi.status() == 1) {
+      time_str = String(DT.year) + "-" + DT.month + "-" + String(DT.day) + ":" + String(DT.hour) + ":" + String(DT.minute) + ":" + String(DT.second);
+      Serial.println(time_str);
+      BackupFile.print(time_str);
+    }
+    else {
+      BackupFile.print(millis());
+    }
+
+    BackupFile.print("\t");
+    // Write Measurement value
+    BackupFile.println(SG);
+
+    Serial.print(F("Writing to SD-card: \t"));
+    Serial.print(time_str);
+    Serial.print("\t");
+    // Write Measurement value
+    Serial.println(SG);
+  }
+  else {
+    Serial.println(F("ERROR: File not found"));
+  }
+  BackupFile.close();
+
+} // Write to sd
+
+
+////////////////////////////// main //////////////////////////////////////////////////////
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
 
