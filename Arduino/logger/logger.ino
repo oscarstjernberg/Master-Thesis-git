cacfdefb7fad42971cfef01989979a646cefd435
DESKTOP-ADSR9DK\Alexander <alexanderlydh@gmail.com>
2018-03-05 12:21:31 +0100

    -

--- Arduino/logger/logger.ino
+++ Arduino/logger/logger.ino
@@ -23,25 +23,25 @@ int status = WL_IDLE_STATUS;
 //          SD SETTINGS           //
 ///////////////////////////////////
 File BackupFile;
-int SD_pin = D4;
+const int SD_pin = D4;
 
-enum months  {Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec};
+enum months { Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec };
 months switch_month;
 struct DateandTime {
-  char month_buf[3];
-  String month;
-  int year;
-  unsigned day;
-  byte hour;
-  byte minute;
-  byte second;
+	char month_buf[3];
+	String month;
+	int year;
+	unsigned day;
+	byte hour;
+	byte minute;
+	byte second;
 } DT;
 
 ////////////////////////////////////
 //      ThingSpeak SETTINGS       //
 ////////////////////////////////////
 
-unsigned long myChannelNumber  = 420692; // Channel ID
+unsigned long myChannelNumber = 420692; // Channel ID
 const char * myWriteAPIKey = "A86F3R21OEZKIIQQ";  // Channel API-key
 
 ////////////////////////////////////
@@ -59,7 +59,7 @@ long t;
 //          MAX31865             //
 ///////////////////////////////////
 
-int pt_pin = D2;
+const int pt_pin = D2;
 
 Adafruit_MAX31865 maxx = Adafruit_MAX31865(D2);
 // Reference value for a PT100 sensor, if using PT1000 use RREF = 4300.
@@ -85,37 +85,40 @@ long interval = 20000;
 int SD_LED = D8;
 int WiFi_LED = D3;
 
+SPISettings settingsA(2000000, MSBFIRST, SPI_MODE1);
+SPISettings settingsB(16000000, LSBFIRST, SPI_MODE3);
+
 //////////////////// WiFi functions /////////////////////
 
-void connectToWiFi(){
-Serial.println();
-Serial.println();
-Serial.print("Connecting to ");
-Serial.println(ssid);
+void connectToWiFi() {
+	Serial.println();
+	Serial.println();
+	Serial.print("Connecting to ");
+	Serial.println(ssid);
 
-WiFi.begin(ssid,pass);
+	WiFi.begin(ssid, pass);
 
-while (WiFi.status() != WL_CONNECTED) {
-delay(500);
-Serial.print(".");
-}
-Serial.println("WiFI connected!");
-digitalWrite(WiFi_LED, HIGH);
-Serial.println();
-Serial.print(ssid);
-Serial.println();
-Serial.println(WiFi.localIP());
-Serial.print("Wifistatus ");
-Serial.println(WiFi.status());
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
+	Serial.println("WiFI connected!");
+	digitalWrite(WiFi_LED, HIGH);
+	Serial.println();
+	Serial.print(ssid);
+	Serial.println();
+	Serial.println(WiFi.localIP());
+	Serial.print("Wifistatus ");
+	Serial.println(WiFi.status());
 }
 
 // WiFiConnection
 void checkWiFiConnection() {
-  if (WiFi.status() != 3) {
-    status = WL_IDLE_STATUS;
-    connectToWiFi();
-    Serial.println("reconnect");
-  }
+	if (WiFi.status() != 3) {
+		status = WL_IDLE_STATUS;
+		connectToWiFi();
+		Serial.println("reconnect");
+	}
 }
 
 //////////////////////// SD-card functions ////////////////////////////////////
@@ -124,74 +127,74 @@ void checkWiFiConnection() {
 
 void SDinit(int SD_pin)
 {
-  Serial.print(F("Initializing SD card..."));
-  if (!SD.begin(SD_pin)) {
-    Serial.println(F("Initialization SD-card failed!"));
-    while (1);
-  }
-  Serial.println(F("Initialization SD-card done."));
+	Serial.print(F("Initializing SD card..."));
+	if (!SD.begin(SD_pin)) {
+		Serial.println(F("Initialization SD-card failed!"));
+		while (1);
+	}
+	Serial.println(F("Initialization SD-card done."));
 
-  // Open file for writing
-  BackupFile = SD.open("Backup.txt", FILE_WRITE);
+	// Open file for writing
+	BackupFile = SD.open("Backup.txt", FILE_WRITE);
 
-  // If the file is open-write
-  if (BackupFile)
-  {
-    BackupFile.print("Time\tMeasurement");
-    BackupFile.println();
-  }
-  else {
-    Serial.println(F("ERROR: File not found"));
-  }
-  BackupFile.close();
+	// If the file is open-write
+	if (BackupFile)
+	{
+		BackupFile.print("Time\tMeasurement");
+		BackupFile.println();
+	}
+	else {
+		Serial.println(F("ERROR: File not found"));
+	}
+	BackupFile.close();
 } // SD_init end
 
 
-// WRITES CONTENT OF SG TO SD CARD  
+  // WRITES CONTENT OF SG TO SD CARD  
 void SDwrite(float SG) {
-  
-  digitalWrite(SD_LED, HIGH); // Indicates that we are writning to SD-card
-  
-  String time_str;
 
-  // Open file for writing
-  BackupFile = SD.open("Backup.txt", FILE_WRITE);
+	digitalWrite(SD_LED, HIGH); // Indicates that we are writning to SD-card
 
-  // If the file is open-write
-  if (BackupFile)
-  {
-    // Write time
-    if (WiFi.status() == 3) {
-      time_str = String(DT.year) + "-" + DT.month + "-" + String(DT.day) + ":" + String(DT.hour) +
-      ":" + String(DT.minute) + ":" + String(DT.second);
-      Serial.println(time_str);
-      BackupFile.print(time_str);
-    }
-    else {
-      BackupFile.print(millis());
-    }
+	String time_str;
 
-    BackupFile.print("\t");
-    // Write Measurement value
-    BackupFile.println(SG);
+	// Open file for writing
+	BackupFile = SD.open("Backup.txt", FILE_WRITE);
 
-    Serial.print(F("Writing to SD-card: \t"));
-    Serial.print(time_str);
-    Serial.print("\t");
-    // Write Measurement value
-    Serial.println(SG);
-  }
-  else {
-    Serial.println(F("ERROR: File not found"));
-  }
-  BackupFile.close();
+	// If the file is open-write
+	if (BackupFile)
+	{
+		// Write time
+		if (WiFi.status() == 3) {
+			time_str = String(DT.year) + "-" + DT.month + "-" + String(DT.day) + ":" + String(DT.hour) +
+				":" + String(DT.minute) + ":" + String(DT.second);
+			Serial.println(time_str);
+			BackupFile.print(time_str);
+		}
+		else {
+			BackupFile.print(millis());
+		}
+
+		BackupFile.print("\t");
+		// Write Measurement value
+		BackupFile.println(SG);
+
+		Serial.print(F("Writing to SD-card: \t"));
+		Serial.print(time_str);
+		Serial.print("\t");
+		// Write Measurement value
+		Serial.println(SG);
+	}
+	else {
+		Serial.println(F("ERROR: File not found"));
+	}
+	BackupFile.close();
 
 } // Write to sd end
 
-////////////////////////////// Load cell functions //////////////////////////////////////////////////////
+  ////////////////////////////// Load cell functions //////////////////////////////////////////////////////
 
-// Initialize load cell
-void loadCellInit(){
+  // Initialize load cell
+void loadCellInit() {
 	ESP.wdtDisable();
 	ESP.wdtEnable(WDTO_8S);
 	delay(0);
@@ -209,7 +212,7 @@ float loadCellRead() {
 	//update() should be called at least as often as HX711 sample rate; >10Hz@10SPS, >80Hz@80SPS
 	//longer delay in scetch will reduce effective sample rate (be carefull with delay() in loop)
 	LoadCell.update();
-  float val;
+	float val;
 	//get smoothed value from data set + current calibration factor
 	if (millis() > t + 250) {
 		val = LoadCell.getData();
@@ -229,119 +232,121 @@ float loadCellRead() {
 	if (LoadCell.getTareStatus() == true) {
 		Serial.println("Tare complete");
 	}
-	
+
 	return	val;
 
 } // loadCellRead end
 
-///////////////////////////////// Temperature function ////////////////////////////////////////////
-
+  ///////////////////////////////// Temperature function ////////////////////////////////////////////
+void tempRead(){
 uint16_t rtd = maxx.readRTD();
 
- Serial.print("RTD value: "); Serial.println(rtd);
- float ratio = rtd;
- ratio /= 32768;
- Serial.print("Ratio = "); Serial.println(ratio,8);
- Serial.print("Resistance = "); Serial.println(RREF*ratio,8);
- Serial.print("Temperature = "); Serial.println(maxx.temperature(RNOMINAL, RREF));
+Serial.print("RTD value: "); Serial.println(rtd);
+float ratio = rtd;
+ratio /= 32768;
+Serial.print("Ratio = "); Serial.println(ratio, 8);
+Serial.print("Resistance = "); Serial.println(RREF*ratio, 8);
+Serial.print("Temperature = "); Serial.println(maxx.temperature(RNOMINAL, RREF));
 
 
-  // Check and print any faults
-  uint8_t fault = maxx.readFault();
-  if (fault) {
-    Serial.print("Fault 0x"); Serial.println(fault, HEX);
-    if (fault & MAX31865_FAULT_HIGHTHRESH) {
-      Serial.println("RTD High Threshold"); 
-    }
-    if (fault & MAX31865_FAULT_LOWTHRESH) {
-      Serial.println("RTD Low Threshold"); 
-    }
-    if (fault & MAX31865_FAULT_REFINLOW) {
-      Serial.println("REFIN- > 0.85 x Bias"); 
-    }
-    if (fault & MAX31865_FAULT_REFINHIGH) {
-      Serial.println("REFIN- < 0.85 x Bias - FORCE- open"); 
-    }
-    if (fault & MAX31865_FAULT_RTDINLOW) {
-      Serial.println("RTDIN- < 0.85 x Bias - FORCE- open"); 
-    }
-    if (fault & MAX31865_FAULT_OVUV) {
-      Serial.println("Under/Over voltage"); 
-    }
-    maxx.clearFault();
-  }
-  Serial.println();
-  
+// Check and print any faults
+uint8_t fault = maxx.readFault();
+if(fault) {
+	Serial.print("Fault 0x"); Serial.println(fault, HEX);
+	if (fault & MAX31865_FAULT_HIGHTHRESH) {
+		Serial.println("RTD High Threshold");
+	}
+	if (fault & MAX31865_FAULT_LOWTHRESH) {
+		Serial.println("RTD Low Threshold");
+	}
+	if (fault & MAX31865_FAULT_REFINLOW) {
+		Serial.println("REFIN- > 0.85 x Bias");
+	}
+	if (fault & MAX31865_FAULT_REFINHIGH) {
+		Serial.println("REFIN- < 0.85 x Bias - FORCE- open");
+	}
+	if (fault & MAX31865_FAULT_RTDINLOW) {
+		Serial.println("RTDIN- < 0.85 x Bias - FORCE- open");
+	}
+	if (fault & MAX31865_FAULT_OVUV) {
+		Serial.println("Under/Over voltage");
+	}
+	maxx.clearFault();
+}
+Serial.println();
+}
 //////////////////////////////// Get time function ///////////////////////////////////////////////
 
-void webTime (Client &client)
+void webTime(Client &client)
 {
-  unsigned long time = 0;
+	unsigned long time = 0;
 
-  // Just choose any reasonably busy web server, the load is really low
-  if (client.connect("g.cn", 80))
-  {
-    // Make an HTTP 1.1 request which is missing a Host: header
-    // compliant servers are required to answer with an error that includes
-    // a Date: header.
-    client.print(F("GET / HTTP/1.1 \r\n\r\n"));
+	// Just choose any reasonably busy web server, the load is really low
+	if (client.connect("g.cn", 80))
+	{
+		// Make an HTTP 1.1 request which is missing a Host: header
+		// compliant servers are required to answer with an error that includes
+		// a Date: header.
+		client.print(F("GET / HTTP/1.1 \r\n\r\n"));
 
-    char buf[5];      // temporary buffer for characters
-    client.setTimeout(5000);
-    if (client.find((char *)"\r\nDate: ") // look for Date: header
-        && client.readBytes(buf, 5) == 5) // discard day name
-    {
+		char buf[5];      // temporary buffer for characters
+		client.setTimeout(5000);
+		if (client.find((char *)"\r\nDate: ") // look for Date: header
+			&& client.readBytes(buf, 5) == 5) // discard day name
+		{
 
-      DT.day = client.parseInt();    // day number
-      client.readBytes(buf, 1);    // discard
-      client.readBytes(DT.month_buf, 3);    // month_buf
-      String str(DT.month_buf);
-      DT.month = str.substring(0, 3);
-      DT.year = client.parseInt();    // year
-      DT.hour = client.parseInt();   // hour
-      DT.minute = client.parseInt(); // minute
-      DT.second = client.parseInt(); // second
+			DT.day = client.parseInt();    // day number
+			client.readBytes(buf, 1);    // discard
+			client.readBytes(DT.month_buf, 3);    // month_buf
+			String str(DT.month_buf);
+			DT.month = str.substring(0, 3);
+			DT.year = client.parseInt();    // year
+			DT.hour = client.parseInt();   // hour
+			DT.minute = client.parseInt(); // minute
+			DT.second = client.parseInt(); // second
 
-      //      }
-    }
-  }
-  delay(10);
-  client.flush();
-  client.stop();
+										   //      }
+		}
+	}
+	delay(10);
+	client.flush();
+	client.stop();
 
 } // end webTime
 
-////////////////////////////// End functions //////////////////////////////////////////////////////
+  ////////////////////////////// End functions //////////////////////////////////////////////////////
 
-////////////////////////////////////
-//          SETUP                 //
-///////////////////////////////////
+  ////////////////////////////////////
+  //          SETUP                 //
+  ///////////////////////////////////
 
 void setup() {
 
-  pinMode(WiFi_LED, OUTPUT);
-  pinMode(SD_LED, OUTPUT);
-  
-  Serial.begin(115200);
-  delay(10);
+	pinMode(WiFi_LED, OUTPUT);
+	pinMode(SD_LED, OUTPUT);
+	pinMode(SD_pin, OUTPUT);
+	pinMode(pt_pin, OUTPUT);
 
-  // Starts the MAX31865 with a 3-wired connection, for 2 or 4 wire connection change to 2WIRE respectly 4Wire
-  maxx.begin(MAX31865_3WIRE);
+	Serial.begin(115200);
+	delay(10);
 
-  // connect to WiFi
-  connectToWiFi();
+	// Starts the MAX31865 with a 3-wired connection, for 2 or 4 wire connection change to 2WIRE respectly 4Wire
+	maxx.begin(MAX31865_3WIRE);
 
-  // Initialize SD-card
-  SDinit(SD_pin);
+	// connect to WiFi
+	connectToWiFi();
 
-  // Initialize reading the load cell
-  loadCellInit();
+	// Initialize SD-card
+	SDinit(SD_pin);
 
-  // ThingSpeak setup
-  ThingSpeak.begin(client);
+	// Initialize reading the load cell
+	loadCellInit();
 
-  // Starts SPI protocol
-  SPI.begin();
+	// ThingSpeak setup
+	ThingSpeak.begin(client);
+
+	// Starts SPI protocol
+	SPI.begin();
 
 }
 
@@ -351,44 +356,44 @@ void setup() {
 
 void loop() {
 
-  
-  unsigned long timeMillis = millis();
+
+	unsigned long timeMillis = millis();
 
 
-  SPI.beginTransaction(pt_pin);
-  // read value from PT100/PT1000 sensor
-  tempRead();
-  SPI.endTransaction();
-  
-  // Check WiFi connection
-  //checkWiFiConnection();
+	SPI.beginTransaction(pt_pin);
+	// read value from PT100/PT1000 sensor
+	tempRead();
+	SPI.endTransaction();
 
-  // Get current time from the web
-  if(WiFi.status()==3){
-  webTime(client);
-  }
+	// Check WiFi connection
+	//checkWiFiConnection();
+
+	// Get current time from the web
+	if (WiFi.status() == 3) {
+		webTime(client);
+	}
 
 	// read value from load cell
 	float val = loadCellRead();
 
 
-  SPI.beginTransaction(SD_pin);
+	SPI.beginTransaction(SD_pin);
 	// Write to SD-card
-  digitalWrite(SD_LED, LOW);
-  delay(1000);
+	digitalWrite(SD_LED, LOW);
+	delay(1000);
 	SDwrite(val);
-  SPI.endTransaction();
+	SPI.endTransaction();
 
-  
+
 	// send to thingspeak
-  if((timeMillis - previousMillis) >= 20000 && WiFi.status()==3){
-  previousMillis = timeMillis;
-  Serial.print("Sending to ThingSpeak ");
-  Serial.println(val);
-  ThingSpeak.setField(1,val);
-  ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
-  }
-  
-  
+	if ((timeMillis - previousMillis) >= 20000 && WiFi.status() == 3) {
+		previousMillis = timeMillis;
+		Serial.print("Sending to ThingSpeak ");
+		Serial.println(val);
+		ThingSpeak.setField(1, val);
+		ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+	}
+
+
 }
 
