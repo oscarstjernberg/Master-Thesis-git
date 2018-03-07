443f5769e5397d78a950ffe75f2541dd77ac7f51
oscarstjernberg <oscar.stjernberg@gmail.com>
2018-03-07 10:19:27 +0100

    SD logger

--- Data/retriveDataFromThingSpeak.m
+++ Data/retriveDataFromThingSpeak.m
@@ -8,17 +8,17 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Retrives data from field 1 in the ThingSpeak channel.
-data = thingSpeakRead(420692,'Fields',1,'NumPoints',8000,'Timeout',10);
+data = thingSpeakRead(441330,'Fields',1,'NumPoints',8000,'Timeout',10);
 figure(1)
 plot(data)
 figure(2)
 histogram(data)
-[data,time] = thingSpeakRead(420692,'Fields',1,'NumMinutes',length(data));
+[data,time] = thingSpeakRead(441330,'Fields',1,'NumMinutes',length(data));
 
 % Retrives data from field 2 in the ThingSpeak channel
-data1 = thingSpeakRead(420692,'Fields',2,'NumPoints',8000,'Timeout',10);
+data1 = thingSpeakRead(441330,'Fields',2,'NumPoints',8000,'Timeout',10);
 figure(3)
 plot(data1)
 figure(4)
 histogram(data1)
-[data1,time1] = thingSpeakRead(420692,'Fields',2,'NumMinutes',length(data));
+[data1,time1] = thingSpeakRead(441330,'Fields',2,'NumMinutes',length(data));
