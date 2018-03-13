121d201a21b9463b3cbd1fa941a7e09b6af766a4
Alydh <alexanderlydh@gmail.com>
2018-03-13 14:51:19 +0100

    Revert "Added comments"
    This reverts commit 6e97d62bf3bc1d1092d06f3a653cbb539558d7d0.

--- Arduino/Logger/WiFi_Functions.cpp
+++ Arduino/Logger/WiFi_Functions.cpp
@@ -17,7 +17,6 @@ void WiFi_FunctionsClass::checkWiFiConnection(ESP8266WiFiClass &WiFi)
 
 void WiFi_FunctionsClass::connectToWiFi(ESP8266WiFiClass &WiFi)
 {
-		// hej
 	int WIFI_count = 0;
 	Serial.println();
 	Serial.println();
