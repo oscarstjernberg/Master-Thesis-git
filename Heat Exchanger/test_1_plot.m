efc598f9b9e75fe6afe81d0fcf70821b77a0d250
DESKTOP-ADSR9DK\Alexander <alexanderlydh@gmail.com>
2018-05-09 11:18:08 +0200

    -

---
+++ Heat Exchanger/test_1_plot.m
@@ -0,0 +1,16 @@
+close all
+plot((run1pidtuning.Time-run1pidtuning.Time(1))/1000, run1pidtuning.Temp,'linewidth',1.5)
+ylabel("Temperature [C]")
+ylim([15 35])
+hold on
+yyaxis right
+ylim([0 900])
+plot((run1pidtuning.Time-run1pidtuning.Time(1))/1000, run1pidtuning.PID,'linewidth',1.5)
+ylabel("PWM-signal")
+grid on
+legend("Temperature","Control signal")
+set(gca,'fontsize',18)
+set(gca,'LineWidth',2)
+xlabel("Time [s]")
+title("Temperature and control signal")
+xlim([0 350])
